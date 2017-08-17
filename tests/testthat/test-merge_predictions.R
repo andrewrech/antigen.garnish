cffaaa3d9d936c3550b67a12a4d6ddfb252206c5
leeprichman <leepr@upenn.edu>
2017-08-17 15:42:03 -0400

    Add files via upload

--- tests/testthat/test-merge_predictions.R
+++ tests/testthat/test-merge_predictions.R
@@ -9,11 +9,11 @@ testthat::test_that("garnish_predictions", {
   # load test data and run test
   # requires mhcflurry output in disk
   file.copy(from = system.file("extdata",
-                               "ag_ex_mhcnuggets_output_gru_HLA-A0203_fc5e83cf-8db7-4033.csv",
+                               "ag_ex_mhcnuggets_output_gru_HLA-A0201_fc5e83cf-8db7-4033.csv",
                                package = "antigen.garnish"), to = getwd())
   
   file.copy(from = system.file("extdata",
-                               "ag_ex_mhcnuggets_output_lstm_HLA-A0203_2d23d1d7-5622-4930.csv",
+                               "ag_ex_mhcnuggets_output_lstm_HLA-A0201_2d23d1d7-5622-4930.csv",
                                package = "antigen.garnish"), to = getwd())
   
   
@@ -33,12 +33,12 @@ testthat::test_that("garnish_predictions", {
   testthat::expect_equal(dto %>% nrow, 552)
   testthat::expect_equal(dto %>% length, 110)
   
-  if (file.exists("ag_ex_mhcnuggets_output_gru_HLA-A0203_fc5e83cf-8db7-4033.csv"))
-    file.remove("ag_ex_mhcnuggets_output_gru_HLA-A0203_fc5e83cf-8db7-4033.csv")
+  if (file.exists("ag_ex_mhcnuggets_output_gru_HLA-A0201_fc5e83cf-8db7-4033.csv"))
+    file.remove("ag_ex_mhcnuggets_output_gru_HLA-A0201_fc5e83cf-8db7-4033.csv")
   
   
-  if (file.exists("ag_ex_mhcnuggets_output_lstm_HLA-A0203_2d23d1d7-5622-4930.csv"))
-    file.remove("ag_ex_mhcnuggets_output_lstm_HLA-A0203_2d23d1d7-5622-4930.csv")
+  if (file.exists("ag_ex_mhcnuggets_output_lstm_HLA-A0201_2d23d1d7-5622-4930.csv"))
+    file.remove("ag_ex_mhcnuggets_output_lstm_HLA-A0201_2d23d1d7-5622-4930.csv")
   
   if (file.exists("antigen.garnish_example_mhcflurry_output_68e72a3d-3504-47f2.csv"))
     file.remove("antigen.garnish_example_mhcflurry_output_68e72a3d-3504-47f2.csv")
