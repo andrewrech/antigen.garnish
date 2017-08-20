485fb1a5ad3bda7c7dd27629187fccc61448f2a5
leeprichman <leepr@upenn.edu>
2017-08-20 13:29:37 -0400

    Update antigen.garnish_run.R

--- R/antigen.garnish_run.R
+++ R/antigen.garnish_run.R
@@ -94,10 +94,10 @@ check_pred_tools <- function(){
 
 default_path <- paste0(system('echo $HOME', intern = TRUE),
                 c(
-                "/antigen.garnish/netMHC-4.0",
-                "/antigen.garnish/netMHCII-2.2",
-                "/antigen.garnish/netMHCIIpan-3.1",
-                "/antigen.garnish/netMHCpan-3.0")) %>%
+                "/antigen.garnish/netMHC/netMHC-4.0",
+                "/antigen.garnish/netMHC/netMHCII-2.2",
+                "/antigen.garnish/netMHC/netMHCIIpan-3.1",
+                "/antigen.garnish/netMHC/netMHCpan-3.0")) %>%
                 paste(collapse = ":")
 
 PATH_status <- list(
