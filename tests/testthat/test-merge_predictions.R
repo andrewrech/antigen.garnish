31916d8f3f20b02f40de3d0a6a251f045133f1dd
leeprichman <leepr@upenn.edu>
2017-08-20 12:51:43 -0400

    Add files via upload

--- tests/testthat/test-merge_predictions.R
+++ tests/testthat/test-merge_predictions.R
@@ -8,22 +8,13 @@ testthat::test_that("garnish_predictions", {
   
   # load test data and run test
   # requires mhcflurry output in disk
-  file.copy(from = system.file("extdata",
-                               "ag_ex_mhcnuggets_output_gru_HLA-A0201_fc5e83cf-8db7-4033.csv",
-                               package = "antigen.garnish"), to = getwd())
-  
-  file.copy(from = system.file("extdata",
-                               "ag_ex_mhcnuggets_output_lstm_HLA-A0201_2d23d1d7-5622-4930.csv",
-                               package = "antigen.garnish"), to = getwd())
-  
-  
-  file.copy(from = system.file("extdata",
-                               "antigen.garnish_example_mhcflurry_output_68e72a3d-3504-47f2.csv",
-                               package = "antigen.garnish"), to = getwd())
-  
-  file.copy(from = system.file("extdata",
-                               "antigen.garnish_example_mhcflurry_output_6e7d571f-8b47-42c5.csv",
-                               package = "antigen.garnish"), to = getwd())
+  urls <- paste0("http://get.rech.io/", c("antigen.garnish_example_mhcnuggets_output_gru_HLA-A0201_fc5e83cf-8db7-4033.csv",
+                        "antigen.garnish_example_mhcnuggets_output_lstm_HLA-A0201_2d23d1d7-5622-4930.csv",
+                        "antigen.garnish_example_mhcflurry_output_6e7d571f-8b47-42c5.csv",
+                        "antigen.garnish_example_mhcflurry_output_68e72a3d-3504-47f2.csv"))
+  for (i in urls){
+      data.table::fread(i) %>% data.table::fwrite(file = basename(i))
+  }
   
   dto <- merge_predictions(
     readRDS(gzcon(url("http://get.rech.io/antigen.garnish_netMHC_test_output.RDS"))),
@@ -31,20 +22,10 @@ testthat::test_that("garnish_predictions", {
   )
   
   testthat::expect_equal(dto %>% nrow, 552)
-  testthat::expect_equal(dto %>% length, 110)
+  testthat::expect_equal(dto %>% length, 111)
   
-  if (file.exists("ag_ex_mhcnuggets_output_gru_HLA-A0201_fc5e83cf-8db7-4033.csv"))
-    file.remove("ag_ex_mhcnuggets_output_gru_HLA-A0201_fc5e83cf-8db7-4033.csv")
-  
-  
-  if (file.exists("ag_ex_mhcnuggets_output_lstm_HLA-A0201_2d23d1d7-5622-4930.csv"))
-    file.remove("ag_ex_mhcnuggets_output_lstm_HLA-A0201_2d23d1d7-5622-4930.csv")
-  
-  if (file.exists("antigen.garnish_example_mhcflurry_output_68e72a3d-3504-47f2.csv"))
-    file.remove("antigen.garnish_example_mhcflurry_output_68e72a3d-3504-47f2.csv")
-  
-  if (file.exists("antigen.garnish_example_mhcflurry_output_6e7d571f-8b47-42c5.csv"))
-    file.remove("antigen.garnish_example_mhcflurry_output_6e7d571f-8b47-42c5.csv")
-  
+  for (i in urls){
+      if (file.exists(basename(i)))
+    file.remove(basename(i))
+  }
 })
-
