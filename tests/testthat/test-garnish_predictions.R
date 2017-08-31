ecbc54c084c7c2bc4d30fe09138930fd8aa4b8dc
leeprichman <leepr@upenn.edu>
2017-08-31 14:15:06 -0400

    Add files via upload

--- tests/testthat/test-garnish_predictions.R
+++ tests/testthat/test-garnish_predictions.R
@@ -41,6 +41,46 @@ testthat::test_that("garnish_predictions vcf", {
 
     })
 
+testthat::test_that("garnish_predictions from file", {
+  
+  if (!check_pred_tools() %>% unlist %>% all){
+    testthat::skip("Skipping run_netMHC because prediction tools are not in PATH")
+  }
+  
+  # load test data
+  temp <- data.table::data.table(
+    sample_id = "test",
+    ensembl_transcript_id =
+      c("ENSMUST00000128119",
+        "ENSMUST00000044250",
+        "ENSMUST00000018743"),
+    cDNA_change = c("c.4988C>T",
+                    "c.1114T>G",
+                    "c.718T>A"),
+    MHC = c("HLA-A*02:01 HLA-DRB1*14:67",
+            "H-2-Kb H-2-IAd",
+            "HLA-A*01:47 HLA-DRB1*03:08"))
+  
+  data.table::fwrite(temp, file = "temp.txt", sep = "\t", row.names = FALSE, quote = FALSE)
+  dt <- garnish_predictions(path = "temp.txt")
+  
+  
+  testthat::expect_equal(dt %>% nrow, 552)
+  testthat::expect_equal(dt$MHC %>% unique %>% sort,
+                         c("H-2-IAd",
+                           "H-2-Kb",
+                           "HLA-A*01:47",
+                           "HLA-A*02:01",
+                           "HLA-DRB1*03:08",
+                           "HLA-DRB1*14:67"))
+  testthat::expect_equal(
+    dt[, nmer %>%
+         nchar %>%
+         unique] %>% sort,
+    8:15)
+  if(file.exists("temp.txt")){file.remove("temp.txt")}
+})
+
 testthat::test_that("garnish_predictions peptide assemble", {
 
    if (!check_pred_tools() %>% unlist %>% all){
