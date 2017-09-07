94750f4b11c4df8ce5fe6d68ec0ae9f925a5150d
leeprichman <leepr@upenn.edu>
2017-09-07 16:22:21 -0400

    Update antigen.garnish_predict.R

--- R/antigen.garnish_predict.R
+++ R/antigen.garnish_predict.R
@@ -972,8 +972,9 @@ if (generate){
     sink(file = "/dev/null")
     nmer_dt <- get_nmers(basepep_dt) %>% .[, nmer_l := nmer %>% nchar]
     sink()
-    dt <- data.table::fintersect(dt, nmer_dt,
-       all = TRUE)
+    dt <- merge(dt, nmer_dt,
+            by = intersect(names(dt), names(nmer_dt),
+            all.x = TRUE)
 
     ## drop out single wt nmer from rolling window over fusion peptides from JAFFA input
     if("fus_tx" %chin% names(dt)) dt <- dt %>%
