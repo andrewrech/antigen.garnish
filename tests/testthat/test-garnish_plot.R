50872d9d873b702b78763fe6503654cdd00ae38b
leeprichman <leepr@upenn.edu>
2017-10-02 21:13:18 -0400

    Add files via upload

---
+++ tests/testthat/test-garnish_plot.R
@@ -0,0 +1,27 @@
+library(testthat)
+library(antigen.garnish)
+library(data.table)
+library(magrittr)
+library(dt.inflix)
+library(ggplot2)
+library(cowplot)
+
+testthat::test_that("garnish_plot", {
+  
+  on.exit(list.files(pattern = "(antigen.garnish.*\\.pdf)|(antigen.garnish_example_jaffa)") %>% file.remove)
+  
+  # load test data
+  dt <- data.table::fread("http://get.rech.io/antigen.garnish_example_jaffa_output.txt")
+  
+  jdt <- dt[pep_type == "fus"]
+  
+  dt <- dt[pep_type != "fus"]
+  
+  g <- garnish_plot(dt = dt, jdt = jdt)
+  
+  # run test
+  testthat::expect_equal(list.files(pattern = "\\.pdf") %>% length, 3)
+  testthat::expect_equal(length(g), 3)
+  testthat::expect_equal(class(g[[1]])[1], "gg")
+  
+})
--- tests/testthat/test-garnish_summary.R
+++ tests/testthat/test-garnish_summary.R
@@ -13,16 +13,18 @@ testthat::test_that("garnish_summary", {
 
   # run test
      testthat::expect_equal(dto$nmers, 154)
-     testthat::expect_equal(dto$mhc_binders, 7)
+     testthat::expect_equal(dto$mhc_binders_class_I, 7)
      testthat::expect_equal(dto$variants, 2)
      testthat::expect_equal(dto %>% names,
       c("sample_id",
-        "priority_neos",
-        "classic_neos",
-        "classic_top_score",
-        "alt_neos",
-        "alt_top_score",
-        "mhc_binders",
+        "priority_neos_class_I", "priority_neos_class_II",
+        "classic_neos_class_I", "classic_neos_class_II",
+        "classic_top_score_class_I", "classic_top_score_class_II",
+        "alt_neos_class_I", "alt_neos_class_II",
+        "alt_top_score_class_I", "alt_top_score_class_II",
+        "fs_neos_class_I", "fs_neos_class_II",
+        "fusion_neos_class_I", "fusion_neos_class_II",
+        "mhc_binders_class_I", "mhc_binders_class_II",
         "predictions",
         "nmers",
         "variants",
