aafb53041fd9b1c094b35933d9cf3e6911628590
leeprichman <leepr@upenn.edu>
2017-10-04 14:37:32 -0400

    Update test-garnish_plot.R

--- tests/testthat/test-garnish_plot.R
+++ tests/testthat/test-garnish_plot.R
@@ -13,15 +13,9 @@ testthat::test_that("garnish_plot", {
   # load test data
     dt <- data.table::fread("http://get.rech.io/antigen.garnish_example_jaffa_output.txt")
 
-    jdt <- dt[pep_type == "fus"] %>% .[, fus_tx := "AAAGGGCCCCTTT"]
-
-    dt <- dt[pep_type != "fus"]
-
-    g <- garnish_plot(dt = dt, jdt = jdt)
+    garnish_plot(dt)
 
   # run test
     testthat::expect_equal(list.files(pattern = "\\.pdf") %>% length, 3)
-    testthat::expect_equal(length(g), 3)
-    testthat::expect_equal(class(g[[1]])[1], "gg")
 
 })
