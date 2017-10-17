fc88c4bd16dac200ddb385f1568377f147b32f1b
andrewrech <andrewrech@gmail.com>
2017-10-17 04:41:23 -0400

    Cover union variant combination case

--- tests/testthat/test-garnish_variants_intersection.R
+++ tests/testthat/test-garnish_variants_intersection.R
@@ -14,10 +14,18 @@ testthat::test_that("garnish_variants intersection", {
       utils::download.file(paste0("http://get.rech.io/", .), .)
 
   # run test
-    dto <- garnish_variants(c("antigen.garnish_example_mutect2.vcf",
+    dto_i <- garnish_variants(c("antigen.garnish_example_mutect2.vcf",
                      "antigen.garnish_example_strelka.vcf"))
 
-  testthat::expect_equal(dto %>% length, 23)
-  testthat::expect_equal(dto %>% nrow, 64)
+  testthat::expect_equal(dto_i %>% length, 23)
+  testthat::expect_equal(dto_i %>% nrow, 64)
+
+    # run test
+    dto_ni <- garnish_variants(c("antigen.garnish_example_mutect2.vcf",
+                     "antigen.garnish_example_strelka.vcf"),
+            intersect = FALSE)
+
+  testthat::expect_equal(dto_ni %>% length, 23)
+  testthat::expect_equal(dto_ni %>% nrow, 118)
 
     })
