73e96c017668e25c9f5505e76dfee3df84d8c405
Lee Richman <leepr@upenn.edu>
2018-04-02 19:24:21 -0400

    typo, used %chin% on integers

--- R/antigen.garnish_fitness.R
+++ R/antigen.garnish_fitness.R
@@ -196,7 +196,7 @@ dtlo <- lapply(8:15, function(nmerl){
     }
 
     # drop 8, 10:15 mers because python scripts check anchor residues at 2 and 9, not intended for other lengths
-    if (nmerl %chin% c(8, 10:15)) return(NULL)
+    if (nmerl %in% c(8, 10:15)) return(NULL)
 
     db <- dti[, spc %>% unique]
 
