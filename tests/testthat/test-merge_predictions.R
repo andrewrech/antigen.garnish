b13d245f21d8c0ce7cceed5958a479495d5f50d1
Andrew J. Rech <andrewrech@gmail.com>
2018-06-19 11:35:37 -0400

    shorten filenames to avoid R CMD check warning

--- tests/testthat/test-merge_predictions.R
+++ tests/testthat/test-merge_predictions.R
@@ -7,10 +7,10 @@ testthat::test_that("merge_predictions", {
 
   # load test data
     output <- file.path(d, c(
-    "antigen.garnish_example_mhcflurry_output_6e7d571f-8b47-42c5.csv",
-    "antigen.garnish_example_mhcflurry_output_68e72a3d-3504-47f2.csv",
-    "antigen.garnish_example_mhcnuggets_output_gru_HLA-A0201_fc5e83cf-8db7-4033.csv",
-    "antigen.garnish_example_mhcnuggets_output_lstm_HLA-A0201_2d23d1d7-5622-4930.csv"
+    "antigen.garnish_ex_mhcf_out_42c5.csv",
+    "antigen.garnish_ex_mhcf_out_47f2.csv",
+    "antigen.garnish_ex_mhcn_out_gru_HLA-A0201_4033.csv",
+    "antigen.garnish_ex_mhcn_out_lstm_HLA-A0201_4930.csv"
   ))
 
   # put these in home dir for testing
--- tests/testthat/test-run_mhcnuggets.R
+++ tests/testthat/test-run_mhcnuggets.R
@@ -8,10 +8,10 @@ testthat::test_that("run_mhcnuggets", {
 
   # load test data
     input <- file.path(d,
-      c("antigen.garnish_example_mhcnuggets_input_gru_H-2-KB_e2bcc9b8-94fa-412b.csv",
-      "antigen.garnish_example_mhcnuggets_input_gru_HLA-A0201_9efed71c-199b-44b3.csv",
-      "antigen.garnish_example_mhcnuggets_input_lstm_H-2-KB_97739391-6066-4632.csv",
-      "antigen.garnish_example_mhcnuggets_input_lstm_HLA-A0201_e4c2872d-c055-4a93.csv"
+      c("antigen.garnish_ex_mhcn_in_gru_H-2-KB_412b.csv",
+      "antigen.garnish_ex_mhcn_in_gru_44b3.csv",
+      "antigen.garnish_ex_mhcn_in_lstm_H-2-KB_4632.csv",
+      "antigen.garnish_ex_mhcn_in_lstm_HLA-A0201_4a93.csv"
     ))
 
     # temporarily copy these to working directory
