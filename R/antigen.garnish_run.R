f593dd9b96606d1c1cefc71a583bdddc72b54897
andrewrech <rech@rech.io>
2020-04-06 11:01:21 -0400

    Improve logging and re-direc stderr; lintr

--- R/antigen.garnish_run.R
+++ R/antigen.garnish_run.R
@@ -11,21 +11,20 @@
 #' @export SW_align
 #' @md
 SW_align <- function(col1,
-                      col2,
-                      gap_open = -11,
-                      gap_extend = -1){
+                     col2,
+                     gap_open = -11,
+                     gap_extend = -1) {
+  al <- Biostrings::pairwiseAlignment(col1, col2,
+    substitutionMatrix = "BLOSUM62",
+    gapOpening = gap_open,
+    gapExtension = gap_extend,
+    type = "local",
+    scoreOnly = TRUE
+  )
 
-        al <- Biostrings::pairwiseAlignment(col1, col2,
-                                  substitutionMatrix = "BLOSUM62",
-                                  gapOpening = gap_open,
-                                  gapExtension = gap_extend,
-                                  type = "local",
-                                  scoreOnly = TRUE)
+  if (length(al) == 0) al <- as.numeric(NA)
 
-        if (length(al) == 0) al <- as.numeric(NA)
-
-        return(al)
-
+  return(al)
 }
 
 #' Internal function to run run_mhcflurry commands.
@@ -33,17 +32,20 @@ SW_align <- function(col1,
 #' @export run_mhcflurry
 #' @md
 
-run_mhcflurry <- function(){
-
+run_mhcflurry <- function() {
   message("Running mhcflurry in parallel")
 
-    list.files(pattern = "mhcflurry_input.*csv") %>%
+  fn <- list.files(pattern = "mhcflurry_input.*csv")
 
-mclapply(., function(x){
-      paste0("mhcflurry-predict ", x, " --out ", x %>%
-            stringr::str_replace("input", "output")) %>%
-      system
-          })
+  fn %>%
+    seq_along() %>%
+    mclapply(function(x) {
+      message(paste0("Input file ", x, " of ", length(fn)))
+
+      paste0("mhcflurry-predict ", fn[x], " --out ", fn[x] %>%
+        stringr::str_replace("input", "output"), " 2>>mhcflurry-predict.log") %>%
+        system()
+    })
 }
 
 
@@ -54,45 +56,56 @@ mclapply(., function(x){
 #' @export run_mhcnuggets
 #' @md
 
-run_mhcnuggets <- function(){
-
+run_mhcnuggets <- function() {
   gruf <- list.files(pattern = "mhcnuggets_input_gru.*csv")
 
-  if (gruf %>% length > 1)
+  if (gruf %>% length() > 1) {
     message("Running mhcnuggets with -m gru")
+  }
 
-    pypath  <- system("which predict.py", intern = TRUE)
+  pypath <- system("which predict.py", intern = TRUE)
 
-parallel::mclapply(gruf, function(x){
-        paste0("python ", pypath, " -m gru -w ", dirname(pypath), "/../saves/production/mhcnuggets/",
-               stringr::str_extract(string = x, pattern = "(?<=_)H.*(?=_)"), ".h5 -p ", x,
-               " > ",
-               x %>% stringr::str_replace("input", "output")) %>%
-          system
+  gruf %>%
+    seq_along() %>%
+    parallel::mclapply(function(x) {
+      print(paste0("Input file ", x, " of ", length(gruf)))
+
+      paste0(
+        "python ", pypath, " -m gru -w ", dirname(pypath), "/../saves/production/mhcnuggets/",
+        stringr::str_extract(string = gruf[x], pattern = "(?<=_)H.*(?=_)"), ".h5 -p ", gruf[x],
+        " > ",
+        gruf[x] %>% stringr::str_replace("input", "output"), " 2>>mhcnuggets.log"
+      ) %>%
+        system()
 
       return(NULL)
-
-      })
+    })
 
 
 
   lf <- list.files(pattern = "mhcnuggets_input_lstm.*csv")
 
-  if (lf %>% length > 1)
-  message("Running mhcnuggets with -m lstm")
+  if (lf %>% length() > 1) {
+    message("Running mhcnuggets with -m lstm")
+  }
 
-  pypath  <- system("which predict.py", intern = TRUE)
+  pypath <- system("which predict.py", intern = TRUE)
 
-parallel::mclapply(lf, function(x){
-        paste0("python ", pypath, " -m lstm -w ",  dirname(pypath), "/../saves/production/mhcnuggets_beta/",
-               stringr::str_extract(string = x, pattern = "(?<=_)H.*(?=_)"), ".h5 -p ", x,
-               " > ",
-               x %>% stringr::str_replace("input", "output")) %>%
-          system
+  lf %>%
+    seq_along() %>%
+    parallel::mclapply(function(x) {
+      print(paste0("Input file ", lf[x], " of ", length(lf)))
 
-  return(NULL)
+      paste0(
+        "python ", pypath, " -m lstm -w ", dirname(pypath), "/../saves/production/mhcnuggets_beta/",
+        stringr::str_extract(string = lf[x], pattern = "(?<=_)H.*(?=_)"), ".h5 -p ", lf[x],
+        " > ",
+        lf[x] %>% stringr::str_replace("input", "output"), " 2>>mhcnuggets.log"
+      ) %>%
+        system()
 
-      })
+      return(NULL)
+    })
 }
 
 
@@ -105,13 +118,15 @@ parallel::mclapply(lf, function(x){
 #' @export run_netMHC
 #' @md
 
-run_netMHC <- function(dt){
-
-  if (!"command" %chin% (dt %>% names))
+run_netMHC <- function(dt) {
+  if (!"command" %chin% (dt %>% names())) {
     stop("dt must contain command column")
+  }
 
   fn <- paste(Sys.time() %>% stringr::str_replace_all("\\ |-|:", ""),
-  "_netMHC_commands.txt", sep = "")
+    "_netMHC_commands.txt",
+    sep = ""
+  )
 
   scriptn <- fn %>% stringr::str_replace("\\.txt$", ".sh")
 
@@ -121,22 +136,27 @@ run_netMHC <- function(dt){
   set(dt,
     j = "outname",
     value = dt[, filename] %>%
-    stringr::str_replace("\\.csv$", "_o.csv"))
+      stringr::str_replace("\\.csv$", "_o.csv")
+  )
 
 
   dt[, command2 := paste(command,
-                              " > ",
-                              outname,
-                              sep = "")]
+    " > ",
+    outname,
+    sep = ""
+  )]
 
   # write 10 commands per line sep by ;
-  cmdlist <- dt[, .SD %>% unique, .SDcols = "command2"] %>% split(1:(nrow(.) / 10))
+  cmdlist <- dt[, .SD %>% unique(), .SDcols = "command2"] %>% split(1:(nrow(.) / 10))
 
-  cmdlist <- lapply(cmdlist, function(i){
-
-    i %>% unlist %>% paste(collapse =  ";")
-
-  }) %>% unlist %>% unique %>% as.data.table
+  cmdlist <- lapply(cmdlist, function(i) {
+    i %>%
+      unlist() %>%
+      paste(collapse = ";")
+  }) %>%
+    unlist() %>%
+    unique() %>%
+    as.data.table()
 
   cmdlist %>% data.table::fwrite(fn, col.names = FALSE)
 
@@ -147,8 +167,10 @@ run_netMHC <- function(dt){
   # 5000 is a conservative line length to be safe, could go much higher  probably
   # memory is not protected here, user will need to chunk if RAM limited
   cat(
-    paste("parallel -s 5000 --jobs 90% --delay 0.2 --no-run-if-empty --joblog ",
-      plog, " < ", fn, sep = ""),
+    paste("parallel --eta -s 5000 --jobs 90% --delay 0.2 --no-run-if-empty --joblog ",
+      plog, " < ", fn,
+      sep = ""
+    ),
     file = scriptn,
     fill = TRUE
   )
@@ -159,30 +181,31 @@ run_netMHC <- function(dt){
   shpath <- system("which sh", intern = TRUE)
 
   # replace whitespace if shpath is empty to let default shell run it
-  cmd  <- paste(shpath, scriptn, sep = " ") %>%
-  stringr::str_replace("^\\ ", "./")
+  cmd <- paste(shpath, scriptn, sep = " ") %>%
+    stringr::str_replace("^\\ ", "./")
 
   # parallel will run commands with whatever shell script is invoked with
-  exstat <- system(cmd, wait = TRUE) %>% unlist
+  exstat <- system(cmd, wait = TRUE) %>% unlist()
 
   outfiles <- dt[, outname]
 
   logf <- data.table::fread(plog)
 
   # check all commands exited with 0 from parallel logfile
-  if (any(logf[, Exitval != 0])){
-
-    cmd <- logf[Exitval != 0, Command] %>% paste(collapse  =  "\n")
+  if (any(logf[, Exitval != 0])) {
+    cmd <- logf[Exitval != 0, Command] %>% paste(collapse = "\n")
 
     stop(paste("netMHC errored on a command, check MHC syntax with `list_mhc()`",
-    "and make sure netMHC is in path. Errored command was:",
-    cmd, sep = " "))
-
+      "and make sure netMHC is in path. Errored command was:",
+      cmd,
+      sep = " "
+    ))
   }
 
   # check for expected output files from parallel run
-  if (!all(file.exists(outfiles)))
+  if (!all(file.exists(outfiles))) {
     stop("netMHC did not produce output for all commands.")
+  }
 
   # just in case files get created and parallel logfile doesnt catch error, check here.
   if (exstat != 0) stop("Exit status from running netMHC parallel script is not zero.")
@@ -194,16 +217,13 @@ run_netMHC <- function(dt){
   file.remove(plog)
 
   # generate command/output file list
-  esl <- lapply(1:nrow(dt), function(n){
-
+  esl <- lapply(1:nrow(dt), function(n) {
     return(list(dt[n][, command], dt[n][, outname]))
-
   })
 
-  dtl <- esl %>% collate_netMHC
+  dtl <- esl %>% collate_netMHC()
 
-      return(dtl)
-
+  return(dtl)
 }
 
 
@@ -212,8 +232,8 @@ run_netMHC <- function(dt){
 #' @export ag_data_err
 #' @md
 
-ag_data_err <- function(){
-    err <- paste(
+ag_data_err <- function() {
+  err <- paste(
     "",
     "Unable to locate antigen.garnish data directory,",
     "or directory contents are missing.",
@@ -231,10 +251,9 @@ ag_data_err <- function(){
     "Documentation:",
     "https://neoantigens.io",
     sep = "\n"
-    )
+  )
 
   stop(err)
-
 }
 
 #' Internal function to configure netMHC suite of tools
@@ -244,40 +263,45 @@ ag_data_err <- function(){
 #' @export configure_netMHC_tools
 #' @md
 
-configure_netMHC_tools <- function(dir){
-
+configure_netMHC_tools <- function(dir) {
   owd <- getwd()
 
-  on.exit({setwd(owd)})
+  on.exit({
+    setwd(owd)
+  })
 
   # netMHC parent dir in ag data dir
   npd <- file.path(dir, "netMHC")
 
-  if (!dir.exists(npd))
+  if (!dir.exists(npd)) {
     stop("netMHC parent directory in antigen.garnish data directory not found.")
+  }
 
   # get path to scripts
   setwd(npd)
 
   tool_paths <- list.files()
 
-  f <- lapply(tool_paths, function(i){
+  f <- lapply(tool_paths, function(i) {
 
     # pattern to find netMHC scripts
-    list.files(i, pattern = "netMHC(II)?(pan)?-?([0-9]\\.[0-9])?$",
-        full.names = TRUE, ignore.case = TRUE)
-
-  }) %>% unlist
+    list.files(i,
+      pattern = "netMHC(II)?(pan)?-?([0-9]\\.[0-9])?$",
+      full.names = TRUE, ignore.case = TRUE
+    )
+  }) %>% unlist()
 
   message("Checking netMHC scripts in antigen.garnish data directory.")
   # sed scripts to correct paths
   # install data from DTU
-  io <- lapply(f, function(i){
+  io <- lapply(f, function(i) {
 
     # rename to take off version, necessary because commands are built into table
     # in get_pred_commands and check_pred_tools hasn't run at that point
-    io <- file.path(dirname(i),
-    basename(i) %>% stringr::str_replace("-.*$", ""))
+    io <- file.path(
+      dirname(i),
+      basename(i) %>% stringr::str_replace("-.*$", "")
+    )
 
     line <- file.path(dir, "netMHC", dirname(i))
 
@@ -289,8 +313,9 @@ configure_netMHC_tools <- function(dir){
     p1 <- line %>% stringr::str_replace_all("/", "")
 
     # if correct directory is already in the script, skip
-    if (any(grepl(catfile, pattern = p1)))
+    if (any(grepl(catfile, pattern = p1))) {
       return(io)
+    }
 
     # properly escape for sed call
     line <- paste("setenv NMHOME ", line %>% stringr::str_replace_all("/", "\\\\/"), sep = "")
@@ -299,36 +324,37 @@ configure_netMHC_tools <- function(dir){
     # formatting for these links is  not consistent,  try no uname first
     # netMHC is always capitalized in the links it seems
     # dirname(i) will always have version number
-    link_to_data <- paste0("http://www.cbs.dtu.dk/services/",
-        dirname(i) %>% stringr::str_replace("net", "Net"),
-        "/data.tar.gz")
+    link_to_data <- paste0(
+      "http://www.cbs.dtu.dk/services/",
+      dirname(i) %>% stringr::str_replace("net", "Net"),
+      "/data.tar.gz"
+    )
 
     cmd <- paste("curl -fsSL", link_to_data, ">", "dtu.tar.gz")
 
     suppressWarnings({
-    curl_status <- system(cmd, intern = TRUE, ignore.stderr = TRUE)
+      curl_status <- system(cmd, intern = TRUE, ignore.stderr = TRUE)
     })
 
     if (!is.null(attr(curl_status, "status")) &&
-        attr(curl_status, "status") == 22){
-
+      attr(curl_status, "status") == 22) {
       un <- system("uname -s", intern = TRUE)
 
       if (!un %chin% c("Linux")) stop("Linux only.")
 
       cmd <- cmd %>%
-      stringr::str_replace("data\\.tar\\.gz", paste0("data.", un, ".tar.gz"))
+        stringr::str_replace("data\\.tar\\.gz", paste0("data.", un, ".tar.gz"))
 
       message(paste("Downloading data for", dirname(i), "on", un))
 
       suppressWarnings({
-      curl_status <- system(cmd, intern = TRUE, ignore.stderr = TRUE)
+        curl_status <- system(cmd, intern = TRUE, ignore.stderr = TRUE)
       })
-
     }
 
-    if (!file.exists("dtu.tar.gz") || file.info("dtu.tar.gz")$size == 0)
-    stop(paste("Unable to download data tar from DTU. See ReadMe in", file.path(dir, "netMHC", dirname(i))))
+    if (!file.exists("dtu.tar.gz") || file.info("dtu.tar.gz")$size == 0) {
+      stop(paste("Unable to download data tar from DTU. See ReadMe in", file.path(dir, "netMHC", dirname(i))))
+    }
 
     # move it into the right folder and untar
     dtar <- file.path(dirname(i), "dtu.tar.gz")
@@ -347,30 +373,30 @@ configure_netMHC_tools <- function(dir){
     if (!file.exists(bk)) file.rename(i, bk)
 
     cmd <- paste("cat ", bk, " | ",
-    # place holders for variable references
-    "sed 's/\\$NMHOME/placeholderxxx/' | ",
-    "sed 's/\\$TMPDIR/placeholderyyy/' | ",
-    "sed 's/setenv.*NMHOME.*$/", line, "/' | ",
-    # reset placeholder
-    "sed 's/placeholderxxx/$NMHOME/' | ",
-    "sed 's/setenv.*TMPDIR.*$/", line2, "/' | ",
-    # reset placeholder
-    "sed 's/placeholderyyy/$TMPDIR/' > ", io, sep = "")
+      # place holders for variable references
+      "sed 's/\\$NMHOME/placeholderxxx/' | ",
+      "sed 's/\\$TMPDIR/placeholderyyy/' | ",
+      "sed 's/setenv.*NMHOME.*$/", line, "/' | ",
+      # reset placeholder
+      "sed 's/placeholderxxx/$NMHOME/' | ",
+      "sed 's/setenv.*TMPDIR.*$/", line2, "/' | ",
+      # reset placeholder
+      "sed 's/placeholderyyy/$TMPDIR/' > ", io,
+      sep = ""
+    )
 
     system(cmd)
 
     system(paste("chmod u+x", io))
 
     return(io)
-
-  }) %>% unlist
+  }) %>% unlist()
 
   message("Done.")
 
   setwd(owd)
 
   return(io)
-
 }
 
 #' Internal function to check for netMHC tools, mhcflurry and mhcnuggets
@@ -381,92 +407,94 @@ configure_netMHC_tools <- function(dir){
 #' @md
 
 check_pred_tools <- function(ag_dirs = c(
-                                          paste0(
-                                            getwd(),
-                                            "/",
-                                            "antigen.garnish"),
-                                          paste0(
-                                            Sys.getenv("HOME"),
-                                            "/",
-                                            "antigen.garnish")
-                                            )){
+                               paste0(
+                                 getwd(),
+                                 "/",
+                                 "antigen.garnish"
+                               ),
+                               paste0(
+                                 Sys.getenv("HOME"),
+                                 "/",
+                                 "antigen.garnish"
+                               )
+                             )) {
 
 
   # vector of directories to look in for data
 
- if (!Sys.getenv("AG_DATA_DIR") == "")
-   if (!Sys.getenv("AG_DATA_DIR") %>% dir.exists){
+  if (!Sys.getenv("AG_DATA_DIR") == "") {
+    if (!Sys.getenv("AG_DATA_DIR") %>% dir.exists()) {
+      message("$AG_DATA_DIR does not exist")
+      ag_data_err()
+    }
+  }
 
-     message("$AG_DATA_DIR does not exist")
-     ag_data_err()
-
-   }
-
-   if (Sys.getenv("AG_DATA_DIR") %>% dir.exists){
-
-     ag_dir <- Sys.getenv("AG_DATA_DIR")
-
-   }
+  if (Sys.getenv("AG_DATA_DIR") %>% dir.exists()) {
+    ag_dir <- Sys.getenv("AG_DATA_DIR")
+  }
 
 
- if (Sys.getenv("AG_DATA_DIR") == ""){
-
-    for(i in ag_dirs)
-      if (i %>% dir.exists){
+  if (Sys.getenv("AG_DATA_DIR") == "") {
+    for (i in ag_dirs) {
+      if (i %>% dir.exists()) {
         ag_dir <- i
         break
       }
+    }
 
     Sys.setenv(AG_DATA_DIR = ag_dir)
-
-   }
+  }
 
   scripts <- configure_netMHC_tools(ag_dir)
 
-  default_path <- paste0(ag_dir,
-                  c(file.path("/netMHC",
-                  list.files(file.path(ag_dir, "netMHC"))),
-                    "/mhcnuggets/scripts"
-                    )) %>%
-                      normalizePath %>%
-                      paste(collapse = ":")
+  default_path <- paste0(
+    ag_dir,
+    c(
+      file.path(
+        "/netMHC",
+        list.files(file.path(ag_dir, "netMHC"))
+      ),
+      "/mhcnuggets/scripts"
+    )
+  ) %>%
+    normalizePath() %>%
+    paste(collapse = ":")
 
   tool_status <- list(
-                mhcflurry   = TRUE,
-                netMHC      = TRUE,
-                netMHCpan   = TRUE,
-                netMHCII    = TRUE,
-                netMHCIIpan = TRUE,
-                mhcnuggets  = TRUE
-                )
+    mhcflurry   = TRUE,
+    netMHC      = TRUE,
+    netMHCpan   = TRUE,
+    netMHCII    = TRUE,
+    netMHCIIpan = TRUE,
+    mhcnuggets  = TRUE
+  )
 
   # conditional to prevent infinitely growing path when running in long loops
-  if(!grepl(pattern = default_path, x = Sys.getenv("PATH"), fixed = TRUE))
+  if (!grepl(pattern = default_path, x = Sys.getenv("PATH"), fixed = TRUE)) {
     Sys.setenv(PATH = paste0(default_path, ":", Sys.getenv("PATH")))
+  }
 
-    if (suppressWarnings(system('which mhcflurry-predict 2> /dev/null', intern = TRUE)) %>%
-          length == 0){
-          message("mhcflurry-predict is not in PATH\n       Download: https://github.com/hammerlab/mhcflurry")
-        tool_status$mhcflurry <- FALSE
-        }
+  if (suppressWarnings(system("which mhcflurry-predict 2> /dev/null", intern = TRUE)) %>%
+    length() == 0) {
+    message("mhcflurry-predict is not in PATH\n       Download: https://github.com/hammerlab/mhcflurry")
+    tool_status$mhcflurry <- FALSE
+  }
 
-    lapply(scripts, function(i){
+  lapply(scripts, function(i) {
+    f <- basename(i)
 
-      f <- basename(i)
+    if (suppressWarnings(system(paste("which", f, "2> /dev/null"), intern = TRUE)) %>%
+      length() == 0) {
+      message(paste(f, " is not in PATH\n       Download: http://www.cbs.dtu.dk/services/"), sep = "")
+      tool_status[[f]] <- FALSE
+    }
+  })
 
-      if (suppressWarnings(system(paste("which", f, "2> /dev/null"), intern = TRUE)) %>%
-            length == 0){
-              message(paste(f, " is not in PATH\n       Download: http://www.cbs.dtu.dk/services/"), sep = "")
-            tool_status[[f]] <- FALSE
-      }
+  if (suppressWarnings(system("which predict.py 2> /dev/null", intern = TRUE)) %>%
+    length() == 0) {
+    message("mhcnuggets predict.py is not in PATH\n       Download: https://github.com/KarchinLab/mhcnuggets")
+    tool_status$mhcnuggets <- FALSE
+  }
 
-    })
-
-    if (suppressWarnings(system('which predict.py 2> /dev/null', intern = TRUE)) %>%
-          length == 0){
-            message("mhcnuggets predict.py is not in PATH\n       Download: https://github.com/KarchinLab/mhcnuggets")
-          tool_status$mhcnuggets <- FALSE
-         }
-
-          return(tool_status)
+  return(tool_status)
 }
