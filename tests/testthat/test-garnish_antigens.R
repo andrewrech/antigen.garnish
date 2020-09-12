0beb92c34497bc59f876be1397e10f95457c7cad
Lee Richman <leepr@upenn.edu>
2020-09-12 17:58:23 -0400

    update garnish_antigens, test, and docs to reflect TESLA consortium prioritization criteria

--- tests/testthat/test-garnish_antigens.R
+++ tests/testthat/test-garnish_antigens.R
@@ -3,7 +3,7 @@ testthat::test_that("garnish_antigens", {
   d <- test_data_dir()
 
   # load test data
-  dt <- file.path(d, "antigen.garnish_PureCN_example_output.txt") %>%
+  dt <- file.path(d, "antigen.garnish_PureCN_example_output2.txt") %>%
     data.table::fread() %>%
     garnish_antigens()
 
@@ -11,20 +11,30 @@ testthat::test_that("garnish_antigens", {
 
   testthat::expect_equal(dt, structure(
     list(
-      sample_id = c("antigen.garnish_test_pureCN.vcf", "antigen.garnish_test_pureCN.vcf", "antigen.garnish_test_pureCN.vcf"),
-      nmer = c("KLVVVGAVGV", "LICPYMEPI", "QQLEAAAGV"),
-      MHC = c("HLA-A*02:01", "HLA-A*02:01", "HLA-A*02:01"),
-      protein_change = c("p.Gly12Val", "p.Arg769Cys", "p.Pro1426Leu"),
-      cDNA_change = c("c.35G>T", "c.2305C>T", "c.4277C>T"),
-      external_gene_name = c("KRAS", "MTOR", "MTOR"),
-      Ensemble_score = c(323.874201423834, 132.615664199976, 340.278748742299),
-      dissimilarity = c(NA_real_, NA_real_, NA_real_),
-      iedb_score = c(NA, 0.999999999973312, NA),
-      min_DAI = c(3.23053559356532, 12.505756955261, 3.0504938492974),
-      clone_id = c(1L, 2L, 2L),
-      cl_proportion = c(0.583196682780358, 0.277452002724779, 0.277452002724779)
-    ),
-    class = c("data.table", "data.frame"),
-    row.names = c(NA, -3L)
-  ))
+      sample_id = c("antigen.garnish_test_pureCN.vcf",
+      "antigen.garnish_test_pureCN.vcf", "antigen.garnish_test_pureCN.vcf",
+      "antigen.garnish_test_pureCN.vcf", "antigen.garnish_test_pureCN.vcf"),
+      nmer = c("INNKLQQLE", "KLQQLEAAA", "LEAAAGVLE", "LQQLEAAAG", "NKLQQLEAA"),
+      MHC = c("HLA-A*02:01", "HLA-A*02:01", "HLA-A*02:01", "HLA-A*02:01",
+      "HLA-A*02:01"),
+      protein_change = c("p.Pro1426Leu", "p.Pro1426Leu", "p.Pro1426Leu",
+      "p.Pro1426Leu", "p.Pro1426Leu"),
+      cDNA_change = c("c.4277C>T", "c.4277C>T", "c.4277C>T", "c.4277C>T",
+      "c.4277C>T"),
+      external_gene_name = c("MTOR", "MTOR", "MTOR", "MTOR", "MTOR"),
+      clone_id = c(2L, 2L, 2L, 2L, 2L),
+      Ensemble_score = c(33.53742, 12.0809127, 5.09488, 30.55461, 10.77761),
+      dissimilarity = c(0.5, NA, 0, 0.999, 0.4),
+      iedb_score = c(0.666666667, NA, 4.53e-07, 0, NA),
+      min_DAI = c(0.102436914, 2.566746911, 14.10328001, 0.130682372,
+      11.13865477),
+      Recognition_Features = c("binding affinity; foreignness; dissimilarity",
+      "binding affinity", "binding affinity; foreignness; agretopicity",
+      "binding affinity; dissimilarity",
+      "binding affinity; agretopicity; dissimilarity")),
+      row.names = c(NA, -5L),
+      class = c("data.table", "data.frame")
+    )
+  )
+
 })
