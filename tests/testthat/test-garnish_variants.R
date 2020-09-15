6ec1577f8de6334e0fda1cf314ead89fc0d78f0d
andrewrech <rech@rech.io>
2020-09-14 22:53:43 -0400

    Remove old test files and tests

--- tests/testthat/test-garnish_variants.R
+++ tests/testthat/test-garnish_variants.R
@@ -11,16 +11,4 @@ testthat::test_that("garnish_variants", {
   testthat::expect_equal(dt %>% class() %>% .[1], "data.table")
   testthat::expect_equal(dt$cDNA_change, c("c.4988C>T"))
 
-
-  dt <- "antigen.garnish_hg19anno_example.vcf" %>%
-    file.path(d, .) %>%
-
-    # run test
-    garnish_variants()
-
-  testthat::expect_equal(dt %>% class() %>% .[1], "data.table")
-  testthat::expect_equal(
-    dt$ensembl_transcript_id[c(1, 10, 16)],
-    c("ENST00000623167.1", "ENST00000254821.1", "ENST00000424564.1")
-  )
 })
