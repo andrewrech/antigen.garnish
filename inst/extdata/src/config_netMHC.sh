0a6eda4fea9b4345c8c66d14689f6a5d753c4692
andrewrech <rech@rech.io>
2020-12-27 05:29:01 +0000

    Add Dockerfile

---
+++ inst/extdata/src/config_netMHC.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# from the directory containing the four netMHC tarballs and the antigen.garnish
+# data directory (downloaded from the one-line installation script)
+# copy the tarballs into the antigen.garnish directory
+NET_MHC_DIR=/
+ANTIGEN_GARNISH_DIR=/antigen.garnish
+
+cd "$NET_MHC_DIR" || return 1
+
+mkdir -p "$ANTIGEN_GARNISH_DIR/netMHC" || return 1
+
+find . -name "netMHC*.tar.gz" -exec tar xvzf {} -C "$ANTIGEN_GARNISH_DIR/netMHC" \;
+
+chown "$USER" "$ANTIGEN_GARNISH_DIR/netMHC"
+chmod 700 -R "$ANTIGEN_GARNISH_DIR/netMHC"
+
+# antigen.garnish is ready to run in R!
